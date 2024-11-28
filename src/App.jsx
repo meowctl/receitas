783ae75225461e6106ce9db7a6c999ba3f1a6c76
Lu <meowctl@proton.me>
2024-11-28 16:28:50 -0300

    modificar css, adicionar barra

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Cabecalho from "./componentes/Cabecalho"
 import ListaDeTarefas from "./componentes/ListaDeTarefas"
+import "./App.css"
 
 
 function App() {
   return (
     
-    <ListaDeTarefas/>
+    <Cabecalho />
     
   )
 }
