bacb8468001e0651410d53d902b76976d1eb2763
lumi <meowctl@proton.me>
2024-12-04 20:06:50 +0000

    itens no menu em caixa alta

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -3,9 +3,9 @@ import "./Cabecalho.css"
 function Cabecalho() {
     return (
         <div className="cabecalho">
-            <div style={{width: "100%", textAlign: "left"}}>menu</div>
-            <div style={{width: "100%", textAlign: "center"}}>receitas</div>
-            <div style={{width: "100%", textAlign: "right"}}>pesquisa</div>
+            <div style={{width: "100%", textAlign: "left"}}>MENU</div>
+            <div style={{width: "100%", textAlign: "center"}}>RECEITAS</div>
+            <div style={{width: "100%", textAlign: "right"}}>PESQUISA</div>
         </div>
     )
 }
