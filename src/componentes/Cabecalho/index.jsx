19e11d9d5360598fc2415726b35a2cb556000c48
lulu <meowctl@proton.me>
2024-12-10 11:01:28 -0300

    adicionar font awesome

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,12 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import "./Cabecalho.css"
 
 function Cabecalho() {
     return (
         <div className="cabecalho">
             <div className="cabecalho-itens">
-                <div style={{textAlign: "left"}}>MENU</div>
-                <div style={{textAlign: "center"}}>RECEITAS</div>
-                <div style={{textAlign: "right"}}>PESQUISA</div>
+                <span style={{textAlign: "left"}}>
+                    <FontAwesomeIcon icon={faBars} /> MENU
+                </span>
+                <span style={{textAlign: "center"}}>RECEITAS</span>
+                <span style={{textAlign: "right"}}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} /> PESQUISA
+                </span>
             </div>
         </div>
     )
