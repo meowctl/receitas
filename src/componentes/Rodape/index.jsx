35042255e39a968f816a248fa68cb27cdeeeb90c
lulu <meowctl@proton.me>
2024-12-12 10:51:27 -0300

    criar e adicionar rodapé

---
+++ src/componentes/Rodape/index.jsx
@@ -0,0 +1,13 @@
+import "./Rodape.css"
+
+function Rodape() {
+    return (
+        <>
+            <div className="rodape">
+                <span>© 2024 - IFRN SGA</span>
+            </div>
+        </>
+    )
+}
+
+export default Rodape
