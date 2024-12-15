7db55d38868ecd13b257cbb8ee27d6e64f17d1d5
lulu <meowctl@proton.me>
2024-12-15 19:54:15 -0300

    carregar destaques via json

--- src/componentes/Conteudo/index.jsx
+++ src/componentes/Conteudo/index.jsx
@@ -1,16 +1,27 @@
+import { useEffect, useState } from "react"
 import Destaque from "../Destaque"
 import Destaques from "../Destaques"
 import ListaDeTarefas from "../ListaDeTarefas"
 import "./Conteudo.css"
 
 function Conteudo() {
+    let [receitasData, setReceitasData] = useState({
+        "destaques": [],
+    });
+
+    useEffect(() => {
+        fetch("/dados/receitas.json")
+            .then(resp => resp.json())
+            .then(data => setReceitasData(data));
+    }, []);
+
     return (
         <div id="conteudo">
-            <Destaques destaques={[
-                <Destaque titulo="teste 1" key={1} imagem="https://i.panelinha.com.br/i1/bk-5395-cp-2019-01-03-11375.webp" />,
-                <Destaque titulo="teste 2" key={2} imagem="https://editorialtelevisa.brightspotcdn.com/wp-content/uploads/2022/01/arepas-colombianas.jpg" />,
-                <Destaque titulo="teste 3" key={3} imagem="https://static.itdg.com.br/images/640-440/73e6dec9467a2a9c06d6226f807c41cb/file-de-peixe-assado-1-.jpg" />,
-            ]} />
+            <Destaques destaques={
+                receitasData.destaques.map(
+                    (destaque, key) => <Destaque titulo={destaque.titulo} key={key} imagem={destaque.imagem} />
+                )
+            } />
 
             <ListaDeTarefas />
         </div>
--- src/componentes/Destaque/index.jsx
+++ src/componentes/Destaque/index.jsx
@@ -2,13 +2,13 @@ import "./Destaque.css"
 
 function Destaque(props) {
     return (
-        <div className="destaque-item" style={{
+        <a href="#" className="destaque-item" style={{
             backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3)), url(${props.imagem})`,
             backgroundPosition: "center, center",
             backgroundSize: "cover",
         }}>
             <span>{props.titulo}</span>
-        </div>
+        </a>
     )
 }
 
