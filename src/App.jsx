8fb753b936e2509d4bef06c56e9e8101c967d3c3
Lulu <meowctl@proton.me>
2024-12-16 14:26:12 -0300

    carregar receitas no app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import Home from "./componentes/Home"
 import "./App.css"
 import { HashRouter, Routes, Route } from "react-router-dom"
 import Layout from "./componentes/Layout"
-
+import { useEffect, useState } from "react"
 
 function App() {
+  let [receitasData, setReceitasData] = useState({
+    "destaques": [],
+    "receitas": [],
+  });
+
+  useEffect(() => {
+    fetch("/dados/receitas.json")
+      .then(resp => resp.json())
+      .then(data => setReceitasData(data));
+  }, []);
+
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<Home receitasData={receitasData} />} />
         </Route>
       </Routes>
     </HashRouter>
--- src/componentes/Home/index.jsx
+++ src/componentes/Home/index.jsx
@@ -5,27 +5,16 @@ import Artigos from "../Artigos";
 import ArtigosEntry from "../ArtigosEntry";
 import Conteudo from "../Conteudo";
 
-function Home() {
-    let [receitasData, setReceitasData] = useState({
-        "destaques": [],
-        "receitas": [],
-    });
-
-    useEffect(() => {
-        fetch("/dados/receitas.json")
-            .then(resp => resp.json())
-            .then(data => setReceitasData(data));
-    }, []);
-
+function Home(props) {
     return (
         <Conteudo items={[
             <Destaques key={0} destaques={
-                receitasData.destaques.map(
+                props.receitasData.destaques.map(
                     (destaque, key) => <Destaque titulo={destaque.titulo} key={key} imagem={destaque.imagem} />
                 )
             } />,
             <Artigos key={1} artigos={
-                receitasData.receitas.map(
+                props.receitasData.receitas.map(
                     (artigo, key) => <ArtigosEntry titulo={artigo.titulo} key={key} imagem={artigo.imagem} />
                 )
             } />
